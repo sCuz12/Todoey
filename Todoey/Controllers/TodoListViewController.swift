e175e53883c073df7c669567445b37fc66f8d144
Giorgos Hadjisavvas <georgeomo9@yahoo.gr>
2019-01-02 21:25:39 +0200

    Convert to MVC Design pattern

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    var itemArray = ["Find Mike", "By eggos","Destroy"]
+    var itemArray = [Item]()
     
     let defaults = UserDefaults.standard
     
@@ -18,16 +18,43 @@ class TodoListViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Buy food"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Study"
+        itemArray.append(newItem3)
+       
+        
+        
         //set the item array the array of user defaults
-        if let items = defaults.array(forKey: "TodoListArray") as? [String]{
-            itemArray = items
+    
+     if let items = defaults.array(forKey: "TodoListArray") as? [Item]{
+         itemArray = items
         }
+        
     }
     
     //MARK - Tableview datasource methods
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        //creating constant
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        //ternary operator
+        //value = condition ? valueIfTrue : valueIfFalse
+        cell.accessoryType = item.done == true ? .checkmark : .none
+    
+        
         return cell
         
         
@@ -41,13 +68,20 @@ class TodoListViewController: UITableViewController {
     //MARK - TableView Delagate method
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print(itemArray[indexPath.row])
+    
+    
+//checks if has already checkmark it removes it else it inserts a checkmark
+itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
-        //checks if has already checkmark it removes it else it inserts a checkmark
-        if  tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-             tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }else {
-             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+//          The bellow code is replaced by the above in a single line (refactoring)
+//        if itemArray[indexPath.row].done == false {
+//            itemArray[indexPath.row].done = true
+//        }else {
+//            itemArray[indexPath.row].done = false
+//        }
+        //calling data source method again
+        tableView.reloadData()
+        
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
@@ -64,7 +98,10 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what will happen once the user clicks the add item button on our UIalert
             
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
+            
             self.defaults.set(self.itemArray,forKey:"TodoListArray")
             //reloas the page to show the data
             self.tableView.reloadData()
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by George Hadjisavvas on 02/01/2019.
+//  Copyright © 2019 George Hadjisavvas. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+    
+}
